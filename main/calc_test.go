e800915913510e23f76192a31e857ba113ce0e55
sabuj.jana <sabuj.jana@flipkart.com>
2022-11-11 23:13:44 +0530

    added basic test table

--- main/calc_test.go
+++ main/calc_test.go
@@ -7,3 +7,22 @@ func TestCalc(t *testing.T) {
 		t.Error("Add func() is broken!!!")
 	}
 }
+
+func TestTableCalculate(t *testing.T) {
+	var tests = []struct {
+		x        int
+		y        int
+		expected int
+	}{
+		{2, 2, 4},
+		{5, 5, 10},
+		{3, 3, 6},
+		{6, 6, 12},
+	}
+
+	for _, test := range tests {
+		if output := add(test.x, test.y); output != test.expected {
+			t.Error("Test failed: {} x, {} y , {} expected, {} received", test.x, test.y, test.expected, output)
+		}
+	}
+}
