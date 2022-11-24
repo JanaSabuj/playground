5db6f48475f550afefe8849491f4d1898ed02d45
sabuj.jana <sabuj.jana@flipkart.com>
2022-11-24 08:25:21 +0530

    basics

--- basics/main.go
+++ basics/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var name string = "Sabuj"
+	var name = "Sabuj"
 	const ttl = 1000
 	fmt.Println(name, ttl)
 
@@ -17,5 +17,16 @@ func main() {
 	fmt.Printf("\nAnnual salary: %g", numr)
 	fmt.Printf("\nAnnual salary: %f", numr)
 	fmt.Printf("\n%s is %d years old", name, age)
+
+	// ops
+	fmt.Println("\n", 10/3)
+	fmt.Println(10.0 / 3.0)
+
+	// Go doesn't support implicit type casting - it only does explicit type casting
+	var f float32 = 4.5
+	var ff = 5.5
+	var no = int(f)
+	var nono = int(ff)
+	fmt.Println(no, nono)
 
 }
